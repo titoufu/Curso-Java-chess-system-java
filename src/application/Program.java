e1f6771b10f052b07c7eb81d5b8315a24ba6cdc9
José Francisco Ribeiro <jribeiro@ufu.br>
2021-12-25 12:28:04 -0300

    First Class:Position

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Position position =new Position(3,4);
+	System.out.println(position);
+
+	}
+
+}
