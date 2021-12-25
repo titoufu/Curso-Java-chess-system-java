7092dff8899ab3cd39d78ee9a7a391d8e88a1583
José Francisco Ribeiro <jribeiro@ufu.br>
2021-12-25 12:14:33 -0300

    Revert "Project created"
    This reverts commit cda6709711e00fbab73d287761076b025480189a.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,9 @@
 package application;
 
-import boardgame.Position;
-
 public class Program {
 
 	public static void main(String[] args) {
-		Position position =new Position(1,1);
-		System.out.println(position);
-		
-		
-		
+		System.out.println("Hello worls");
 		}
+
 }
